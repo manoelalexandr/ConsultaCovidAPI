f04accdb1da7385057c0cc541b90512d5290efd4
Manoel Alexandre <manoelalexandr@gmail.com>
2023-01-19 11:08:35 -0300

    Add project files.

---
+++ ConsultaCovidAPI/DTO/DadosPainelDTO.cs
@@ -0,0 +1,24 @@
+﻿using LINQtoCSV;
+using ConsultaCovidAPI.Model;
+using ConsultaCovidAPI.DTO;
+
+namespace ConsultaCovidAPI.DTO
+{
+    public class DadosPainelDTO
+    {
+
+        public static void ReadCsvFile()
+        {
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                IgnoreUnknownColumns = true,
+                SeparatorChar = ';',
+                UseFieldIndexForReadingData = false
+            };
+
+            var csvContext = new CsvContext();
+            var dadosPainel = csvContext.Read<DadosPainel>("HIST_PAINEL_COVIDBR_2020_Parte1_16jan2023.csv", csvFileDescription);
+        }
+    }
+}
---
+++ ConsultaCovidAPI/Model/DadosPainel.cs
@@ -0,0 +1,61 @@
+﻿using LINQtoCSV;
+
+namespace ConsultaCovidAPI.Model
+{
+    [Serializable]
+    public class DadosPainel
+    {
+        [CsvColumn(Name ="regiao")]
+        public string? Regiao { get; set; }
+
+        [CsvColumn(Name = "estado")]
+        public string? Estado { get; set; }
+
+        [CsvColumn(Name = "municipio")]
+        public string? Municipio { get; set; }
+
+        [CsvColumn(Name = "coduf")]
+        public int Coduf { get; set; }
+
+        [CsvColumn(Name = "codmun")]
+        public int Codmun { get; set; }
+
+        [CsvColumn(Name = "codRegiaoSaude")]
+        public int CodRegiaoSaude { get; set; }
+
+        [CsvColumn(Name = "nomeRegiaoSaude")]
+        public string? NomeRegiaoSaude { get; set; }
+
+        [CsvColumn(Name = "data", OutputFormat = "dd-MM-yyyy")]
+        public DateTime Data { get; set; }
+
+        [CsvColumn(Name = "semanaEpi")]
+        public int SemanaEpi { get; set; }
+
+        [CsvColumn(Name = "populacaoTCU2019")]
+        public int PopulacaoTCU2019 { get; set; }
+
+        [CsvColumn(Name = "casosAcumulado")]
+        public int CasosAcumulado { get; set; }
+
+        [CsvColumn(Name = "casosNovos")]
+        public int CasosNovos { get; set; }
+
+        [CsvColumn(Name = "obitosAcumulado")]
+        public int ObitosAcumulado { get; set; }
+
+        [CsvColumn(Name = "obitosNovos")]
+        public int ObitosNovos { get; set; }
+
+        [CsvColumn(Name = "recuperadosnovos")]
+        public int Recuperadosnovos { get; set; }
+
+        [CsvColumn(Name = "emAcompanhamentoNovos")]
+        public int EmAcompanhamentoNovos { get; set; }
+
+        [CsvColumn(Name = "interior/metropolitana")]
+        public byte InteriorMetropolitana { get; set; }
+
+}
+    
+}
